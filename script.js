66bb8ea4e48f9362047615f22217843375598c4e
AKSHAYAA.D <92520985+Akshayaa25@users.noreply.github.com>
2023-01-05 21:39:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+var counter = 1;
+        setInterval(function () {
+            document.getElementById('radio' + counter).checked = true;
+            counter++;
+            if (counter > 3) {
+                counter = 1;
+            }
+        }, 5000);
+
+window.onscroll = function () { changeLogo() };
+function changeLogo() {
+    var logo = document.getElementById("logo");
+    if (document.documentElement.scrollTop) {
+        logo.src = "media/logo.png";
+        logo.style.width = "30px";
+    }
+    else {
+        logo.src = "media/Rolls-Royce_Motor_Cars_logo white.png";
+        logo.style.width = "20px";
+    }
+}
+
+function openMenu() {
+    document.getElementById("sidebar").style.display = "block";
+    document.getElementById("menu").style.visibility = "hidden";
+}
+
+function closeMenu() {
+    document.getElementById("sidebar").style.display = "none";
+    document.getElementById("menu").style.visibility = "visible";
+}
+
+function openSubList() {
+    t = document.getElementsByClassName("list");
+    for (i = 0; i < t.length; i++) {
+        t[i].style.display = "block";
+    }
+}
+function hideSubList() {
+    t = document.getElementsByClassName("list");
+    for (i = 0; i < t.length; i++) {
+        t[i].style.display = "none";
+    }
+}
+
+function next(t) {
+    var elm = t.parentElement.parentElement.children[1].children[0];
+    var item = elm.getElementsByClassName("item");
+    elm.append(item[0]);
+}
+function prev(t) {
+    var elm = t.parentElement.parentElement.children[1].children[0];
+    var item = elm.getElementsByClassName("item");
+    elm.prepend(item[item.length - 1]);
+}
