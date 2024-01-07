2af8d5751546808149e80a8b34d2da59ecc77086
Akshayaa <akshayaa2509@gmail.com>
2024-01-07 12:41:09 +0530

    update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var counter = 1;
-        setInterval(function () {
-            document.getElementById('radio' + counter).checked = true;
-            counter++;
-            if (counter > 3) {
-                counter = 1;
-            }
-        }, 5000);
+setInterval(function () {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 3) {
+        counter = 1;
+    }
+}, 5000);
 
 window.onscroll = function () { changeLogo() };
 function changeLogo() {
